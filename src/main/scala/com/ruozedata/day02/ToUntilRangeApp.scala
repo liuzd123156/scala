497666aa479c4056c53d3ce4485917d9555c7721
liuzd123156 <1044095365@qq.com>
2018-07-23 16:26:40 +0800

    修订

--- src/main/scala/com/ruozedata/day02/ToUntilRangeApp.scala
+++ src/main/scala/com/ruozedata/day02/ToUntilRangeApp.scala
@@ -17,8 +17,13 @@ object ToUntilRangeApp {
         println(1 until(10))
 
         /*
-
+            Range(arg1,arg2,arg3)：arg1是开始，arg2是结束，arg3是步长，左闭右开
+            Range(1,10)输出结果：Range(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            Range(1,10,1)输出结果：Range(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            Range(1,10,2)输出结果：Range(1, 3, 5, 7, 9)
          */
-        println(Range())
+        println(Range(1,10))
+        println(Range(1,10,1))
+        println(Range(1,10,2))
     }
 }
